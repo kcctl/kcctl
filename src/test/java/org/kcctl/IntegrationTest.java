578ce5b9edf1c5ddc87db58956c3acff74ff1dd4
Iskandar Abudiab <mail@iabudiab.dev>
2022-01-10 02:11:02 +0100

    Provide helper utilities for integration tests
    - Add helper method for registering a test connector using Debezium
    Container
    - Add lifecycle hook for injecting a command context into subclasses
    containing the desired command instance to reduce boilerplate
    - Add Quarkus Test Profile

--- src/test/java/org/kcctl/IntegrationTest.java
+++ src/test/java/org/kcctl/IntegrationTest.java
@@ -15,24 +15,32 @@
  */
 package org.kcctl;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.reflect.*;
 import java.nio.file.Files;
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.api.extension.ExtensionConfigurationException;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.platform.commons.util.ReflectionUtils;
+import org.junit.platform.commons.util.ReflectionUtils.HierarchyTraversalMode;
+import org.kcctl.support.InjectCommandContext;
+import org.kcctl.support.KcctlCommandContext;
 import org.kcctl.util.ConfigurationContext;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.lifecycle.Startables;
 import org.testcontainers.utility.DockerImageName;
 
+import io.debezium.testing.testcontainers.ConnectorConfiguration;
 import io.debezium.testing.testcontainers.DebeziumContainer;
+import picocli.CommandLine;
 
-@Tag("integration-test")
 public abstract class IntegrationTest {
 
     protected static final Network network = Network.newNetwork();
@@ -45,22 +53,107 @@ public abstract class IntegrationTest {
             .withKafka(kafka)
             .dependsOn(kafka);
 
-    protected ConfigurationContext configurationContext;
-
     @BeforeAll
-    static void startContainers() {
+    public static void prepare() {
         Startables.deepStart(Stream.of(kafka, kafkaConnect)).join();
     }
 
     @BeforeEach
-    void setupContext(@TempDir File tempDir) throws IOException {
-        var configFile = tempDir.toPath().resolve(".kcctl");
+    void injectCommandContext() {
+        ReflectionUtils.findFields(getClass(), it -> it.isAnnotationPresent(InjectCommandContext.class), HierarchyTraversalMode.TOP_DOWN)
+                .forEach(field -> {
+                    try {
+                        KcctlCommandContext<?> commandContext = prepareContext(field);
+                        ReflectionUtils.makeAccessible(field);
+                        field.set(this, commandContext);
+                    }
+                    catch (IllegalAccessException e) {
+                        throw new IntegrationTestException("Couldn't inject KcctlCommandContext", e);
+                    }
+                });
+    }
 
-        Files.writeString(configFile, String.format(
-                "{ \"currentContext\": \"local\", \"local\": { \"cluster\": \"%s\", \"username\": \"testuser\", \"password\": \"testpassword\" }}",
-                kafkaConnect.getTarget())
-        );
+    @AfterEach
+    public void cleanup() {
+        kafkaConnect.deleteAllConnectors();
+    }
 
-        configurationContext = new ConfigurationContext(tempDir);
+    protected void registerTestConnector(String name) {
+        ConnectorConfiguration config = ConnectorConfiguration.create()
+                .with("connector.class", "FileStreamSource")
+                .with("tasks.max", 1)
+                .with("topic", "local-file-source-test")
+                .with("file", "/tmp/test");
+
+        kafkaConnect.registerConnector(name, config);
+    }
+
+    private KcctlCommandContext<?> prepareContext(Field field) {
+        Type type = field.getGenericType();
+        Type genericType = ((ParameterizedType) type).getActualTypeArguments()[0];
+        Class<?> targetCommand = (Class<?>) genericType;
+
+        ensureCaseyCommand(targetCommand);
+
+        var context = initializeConfigurationContext();
+        var command = instantiateCommand(targetCommand, context);
+        var commandLine = new CommandLine(command);
+        var output = new StringWriter();
+        commandLine.setOut(new PrintWriter(output));
+
+        return new KcctlCommandContext<>(command, commandLine, output);
+    }
+
+    private void ensureCaseyCommand(Class<?> targetCommand) {
+        if (!targetCommand.isAnnotationPresent(CommandLine.Command.class)) {
+            throw new IntegrationTestException("KcctlCommandContext should target a type annotated with @CommandLine.Command");
+        }
+    }
+
+    private ConfigurationContext initializeConfigurationContext() {
+        try {
+            var tempDir = Files.createTempDirectory("kcctl-test");
+            var configFile = tempDir.resolve(".kcctl");
+
+            Files.writeString(configFile, String.format("""
+                    {
+                        "currentContext": "local",
+                        "local": {
+                            "cluster": "%s",
+                            "username": "testuser",
+                            "password": "testpassword"
+                        }
+                    }
+                    """, kafkaConnect.getTarget()));
+
+            return new ConfigurationContext(tempDir.toFile());
+        }
+        catch (IOException e) {
+            throw new IntegrationTestException("Couldn't initialize configuration context", e);
+        }
+    }
+
+    private Object instantiateCommand(Class<?> targetCommand, ConfigurationContext configurationContext) {
+        try {
+            Constructor<?> constructor = targetCommand.getDeclaredConstructor(ConfigurationContext.class);
+            return constructor.newInstance(configurationContext);
+        }
+        catch (NoSuchMethodException e) {
+            throw new IntegrationTestException("Unsupported @CommandLine.Command type. Required a single argument constructor accepting a ConfigurationContext");
+        }
+        catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            throw new IntegrationTestException("Couldn't instantiate command of type " + targetCommand, e);
+        }
+    }
+
+    public static class IntegrationTestException extends RuntimeException {
+
+        public IntegrationTestException(String msg) {
+            super(msg);
+        }
+
+        public IntegrationTestException(String msg, Throwable cause) {
+            super(msg, cause);
+        }
     }
 }
