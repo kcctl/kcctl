b42f9f004065fa316c71bb2527e668681db44b47
Siddique Ahmad <siddique.ahmad@gmail.com>
2022-02-09 09:51:10 +0500

    🧪 #102 removed * as anti best practice

--- src/main/java/org/kcctl/service/KafkaConnectApi.java
+++ src/main/java/org/kcctl/service/KafkaConnectApi.java
@@ -18,7 +18,14 @@ package org.kcctl.service;
 import java.util.List;
 import java.util.Map;
 
-import javax.ws.rs.*;
+import javax.ws.rs.POST;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Path;
+
 
 import org.eclipse.microprofile.rest.client.annotation.RegisterClientHeaders;
 import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
