000bfd4e357b1deab373ce3992db9cb890337161
Gunnar Morling <gunnar.morling@googlemail.com>
2022-08-22 17:42:17 +0200

    🔍 #207 Adding plug-in name completion

---
+++ src/main/java/org/kcctl/command/PluginNamesCompletionCandidateCommand.java
@@ -0,0 +1,63 @@
+/*
+ *  Copyright 2021 The original authors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.kcctl.command;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
+import org.kcctl.service.ConnectorPlugin;
+import org.kcctl.service.KafkaConnectApi;
+import org.kcctl.util.ConfigurationContext;
+
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Model.CommandSpec;
+import picocli.CommandLine.Spec;
+
+@Command(name = "plugin-name-completions", hidden = true)
+public class PluginNamesCompletionCandidateCommand implements Runnable {
+
+    private final ConfigurationContext context;
+
+    @Spec
+    private CommandSpec spec;
+
+    @Inject
+    public PluginNamesCompletionCandidateCommand(ConfigurationContext context) {
+        this.context = context;
+    }
+
+    // Hack : Picocli currently require an empty constructor to generate the completion file
+    public PluginNamesCompletionCandidateCommand() {
+        context = new ConfigurationContext();
+    }
+
+    @Override
+    public void run() {
+        KafkaConnectApi kafkaConnectApi = RestClientBuilder.newBuilder()
+                .baseUri(context.getCurrentContext().getCluster())
+                .build(KafkaConnectApi.class);
+
+        List<ConnectorPlugin> plugins = kafkaConnectApi.getConnectorPlugins(false);
+        String classNames = plugins.stream()
+                .map(ConnectorPlugin::clazz)
+                .collect(Collectors.joining(" "));
+
+        spec.commandLine().getOut().println(classNames);
+    }
+}
